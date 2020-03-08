1559396b21cdea820fb57cc1d0ba33611689b210
C. Leow <chienleow1@gmail.com>
2020-03-08 00:14:23 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,7 +5,7 @@ def key_for_min_value(name_hash)
   name_hash.each do |key, value|
     if lowest_value == nil || value < lowest_value
       lowest_value = value
-      key_for_min_value = key
+      key_for_min = key
     end
   end
   key_for_min
